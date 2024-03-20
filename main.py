15e979a7cdc0c45eecee0f051096aaf703be6c75
Amin Zaidi <64020408+aminzaidi1@users.noreply.github.com>
2024-03-20 18:05:49 +0500

    main.py Update

--- main.py
+++ main.py
@@ -18,19 +18,24 @@ if __name__ == '__main__':
         
         end_time = time.time()  # Stop measuring time
         execution_time = end_time - start_time
+
+        # Load and display the input image
+        query_images = [query_image]
+        print("Displaying input image:")
+        display_images(query_images)
         
         print("Best match found:")
         print(f"Individual ID: {best_match_id}")
         print(f"Similarity: {similarity}")
         print(f"Execution Time: {execution_time} seconds")
         
-        # Load and display the input image
-        query_images = [query_image]
-        print("Displaying input image:")
-        display_images(query_images)
         
+        # Display segmentation and normalization process details for the input image
+        print("Displaying segmentation and normalization process details for the input image:")
+        show_details(query_image)
+
         if best_match_id is not None:
-            print(f"Displaying all images of individual {best_match_id}:")
+            print(f"Displaying all images of the matched individual {best_match_id}:")
             individual_images = []
             for filename in os.listdir(dataset_folder):
                 if filename.startswith(best_match_id) and filename.endswith(".jpg"):
@@ -38,10 +43,6 @@ if __name__ == '__main__':
                     img = cv2.imread(filepath)
                     individual_images.append(img)
             display_images(individual_images)
-        
-        # Display segmentation and normalization process details for the input image
-        print("Displaying segmentation and normalization process details for the input image:")
-        show_details(query_image)
 
         # Close all OpenCV windows after processing
         cv2.destroyAllWindows()
